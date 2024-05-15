6751127acf5f52546dab7971652abbb6e5153932
gianluigi7 <gianluigiferraioli7@gmail.com>
2024-05-15 17:49:15 +0200

    Milestone 1

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
 'use strict'
+
+const team = [
+    {
+      nome: 'wayne Barret',
+      ruolo: 'Founder & CEO',
+      foto:  'img/img/wayne-barnett-founder-ceo.jpg',
+    },
+    {
+      nome: 'Angela Carrol',
+      ruolo: 'Chief Editor',
+      foto: 'img/img/angela-lopez-social-media-manager.jpg',
+    },
+    {
+      nome: 'Angela Lopez',
+      ruolo: 'Social Media Manager',
+      foto: 'img/img/angela-lopez-social-media-manager.jpg',
+    },
+    {
+      nome: 'Barbara Ramos',
+      ruolo: 'Graphic Designer',
+      foto: 'img/img/barbara-ramos-graphic-designer.jpg',
+    },
+    {
+      nome: 'Scott Estrada',
+      ruolo: 'Developer',
+      foto: 'img/img/scott-estrada-developer.jpg',
+    },
+    {
+      nome: 'Walter Gordon',
+      ruolo: 'Office Manager',
+      foto: 'img/img/walter-gordon-office-manager.jpg',
+    },
+];
+
+console.log(team);
